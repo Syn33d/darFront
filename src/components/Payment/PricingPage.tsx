0b3a53eebba13e96813e5e4d30101c9de69c1c2b
Syn33d <128500319+Syn33d@users.noreply.github.com>
2024-06-06 20:06:49 +0200

    Add new pricing page

--- src/components/Payment/PricingPage.tsx
+++ src/components/Payment/PricingPage.tsx
@@ -12,24 +12,7 @@ declare global {
 }
 
 function PricingPage() {
-  const [, setIsFirstTable] = useState(true);
-
-  const handleFirstTableClick = () => {
-    setIsFirstTable(true);
-    const firstTableElement = document.getElementById('firstTable') as HTMLInputElement;
-    if (firstTableElement) {
-      firstTableElement.checked = true;
-    }
-  };
-
-  const handleSecondTableClick = () => {
-    setIsFirstTable(false);
-    const secondTableElement = document.getElementById('secondTable') as HTMLInputElement;
-    if (secondTableElement) {
-      secondTableElement.checked = true;
-    }
-  };
-
+  
   return (
     <>
       <Helmet>
@@ -38,28 +21,12 @@ function PricingPage() {
       <div className="mainPricingPage">
         <div className="form_wrapper">
           <input type="radio" className="radio" name="radio" id="firstTable" defaultChecked />
-          <input type="radio" className="radio" name="radio" id="secondTable" />
           <div className="tile">
             <h3 className="firstTable">Yearly</h3>
-            <h3 className="secondTable">Monthly</h3>
           </div>
-
-          <label className="tab firstTable_tab" htmlFor="firstTable" onClick={handleFirstTableClick}>
-            Yearly
-          </label>
-
-          <label className="tab secondTable_tab" htmlFor="secondTable" onClick={handleSecondTableClick}>
-            Monthly
-          </label>
           <span className="shape"></span>
           <div className="form_wrap">
             <div className="form_fild firstTable_form">
-              <stripe-pricing-table pricing-table-id="prctbl_1PO2h2G3h0hR2My9jjbJGE3z"
-                publishable-key="pk_live_51PFyI9G3h0hR2My98cpysUelbjSwFFXf6Td14iADtumZlDXTClojCtvekZ3BjSM3vlfmVGH43KfgdD1MtK3s3c3a00bRIIuK5B">
-              </stripe-pricing-table>
-            </div>
-
-            <div className="form_fild secondTable_form">
               <stripe-pricing-table pricing-table-id="prctbl_1PO2h2G3h0hR2My9jjbJGE3z"
                 publishable-key="pk_live_51PFyI9G3h0hR2My98cpysUelbjSwFFXf6Td14iADtumZlDXTClojCtvekZ3BjSM3vlfmVGH43KfgdD1MtK3s3c3a00bRIIuK5B">
               </stripe-pricing-table>
